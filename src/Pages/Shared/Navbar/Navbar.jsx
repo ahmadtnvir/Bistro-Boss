72b448933e86ae4c541711dfa842eb62573a4783
Tanvir Ahmad <tnvir9@gmail.com>
2024-05-17 11:30:04 +0600

    Project setup, Setup routes, layout and footer, Create simple Top Banner react responsive carousel

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <Link to={'/'}>HOME</Link>
+      </li>
+      <li>
+        <Link to={'/'}>CONTACT US</Link>
+      </li>
+      <li>
+        <Link to={'/'}>DASHBOARD</Link>
+      </li>
+      <li>
+        <Link to={'/'}>OUR MENU</Link>
+      </li>
+      <li>
+        <Link to={'/'}>OUR SHOP</Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-black text-white z-10 bg-opacity-30 max-w-6xl fixed ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Bistro Boss <br /> Restaurant</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {links}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
